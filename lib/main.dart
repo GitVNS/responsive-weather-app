02dda5a22ef4aae04a53c61be1f1bb461a9147fe
VNS <vnsdev129@gmail.com>
2023-08-31 13:33:09 +0530

    removed unused package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'package:device_preview/device_preview.dart';
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:open_weather/presentation/pages/dashboard.dart';
@@ -8,13 +6,6 @@ import 'package:open_weather/core/theme/app_theme.dart';
 void main() {
   runApp(const MainApp());
 }
-
-// void main() => runApp(
-//       DevicePreview(
-//         enabled: !kReleaseMode,
-//         builder: (context) => const MainApp(),
-//       ),
-//     );
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
